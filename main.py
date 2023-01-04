fc6b2f91368cf4fa1fefbf36b60ced614daba326
Борис Сухобрус <bhobrus@gmail.com>
2023-01-04 23:45:20 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import sys
 
 from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTextEdit,
+    QVBoxLayout,
+    QWidget,
+    QLineEdit,
+    QPushButton,
+    QHBoxLayout,
+)
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 
@@ -13,8 +22,38 @@ class Browser(QMainWindow):
         self.view = QWebEngineView(self)
         self.view.load(QUrl("https://www.google.com"))
 
-        # Set the QWebEngineView as the central widget
-        self.setCentralWidget(self.view)
+        # Create the address bar
+        self.address_bar = QLineEdit(self)
+        self.address_bar.returnPressed.connect(self.load_url)
+
+        # Create the back and forward buttons
+        self.back_button = QPushButton("<", self)
+        self.back_button.clicked.connect(self.view.back)
+        self.forward_button = QPushButton(">", self)
+        self.forward_button.clicked.connect(self.view.forward)
+
+        # Create the refresh button
+        self.refresh_button = QPushButton("R", self)
+        self.refresh_button.clicked.connect(self.view.reload)
+
+        # Create the layout
+        layout = QVBoxLayout(self)
+        nav_bar = QHBoxLayout(self)
+        nav_bar.addWidget(self.back_button)
+        nav_bar.addWidget(self.forward_button)
+        nav_bar.addWidget(self.refresh_button)
+        nav_bar.addWidget(self.address_bar)
+        layout.addLayout(nav_bar)
+        layout.addWidget(self.view)
+
+        # Set the layout as the central widget
+        central_widget = QWidget(self)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def load_url(self):
+        url = QUrl(self.address_bar.text())
+        self.view.load(url)
 
 
 if __name__ == "__main__":
